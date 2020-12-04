68cf3ad9eb7b67e227f2db08988b19ca665fe653
loockeeer <lulu.r1313@gmail.com>
2020-12-04 21:25:49 +0100

    Add route import

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ const chalk = require('chalk')
 
 const walk = require('./utils/walk')
 
+// Import routes
+const compareRoute = require('./routes/compare')
+
+// Configure routes
+app.get('/compare', compareRoute)
+
+
+// App main
 async function main () {
   const publicPath = path.join(__dirname, 'public' || process.env.DATA_FOLDER)
   const files = []
 
   console.log(chalk`{bold.blue [LOAD][LOG]} Start computing hash of files`)
-
   const hashStartTime = Date.now()
+
   for await (const file of walk(publicPath)) {
     const hash = await hasha.fromFile(file, { algorithm: 'sha1' })
     files.push({
@@ -22,12 +30,17 @@ async function main () {
       hash
     })
   }
+
   const fileHashTime = Date.now() - hashStartTime
   console.log(
     chalk`{bold.blue [LOAD][LOG]} Files hash computed in {gray ${
       fileHashTime / 1000
     }} seconds`
   )
+
+  app.configure(() => {
+    app.set('files', files)
+  })
 
   console.log(chalk`{bold.blue [STARTING][LOG]} Ready to start server`)
   app.listen(process.env.PORT, () => {
