a26a5c893786728d4c11dcfa619e431d902f34d7
loockeeer <lulu.r1313@gmail.com>
2020-12-04 21:08:22 +0100

    Old version wasn't good

--- src/index.js
+++ src/index.js
@@ -5,83 +5,33 @@ const path = require('path')
 const fs = require('fs').promises
 const tar = require('tar')
 const hasha = require('hasha')
-const fetchAssets = require('./utils/fetchAssets')
-const fetchTars = require('./utils/fetchTars')
+const chalk = require('chalk')
 
-const global = {}
 
-app.get('/assets', (req, res) => {
-  return res.send(
-    global.assets.map((asset) => ({
-      name: asset.name,
-      hash: asset.hash
-    }))
-  )
-})
-
-app.get('/assets/:assetName', (req, res) => {
-  const asset = global.assets.find(asset=>asset.name === req.params.assetName)
-  
-  if(!asset) {
-    return res.status(404).send({message: 'Could not find any asset with name ' + req.params.assetName})
-  }
-  
-  return res.sendFile(asset.tarPath)
-})
+const walk = require('./utils/walk')
 
 async function main () {
-  const assetsPath = path.join(__dirname, 'assets')
+  const publicPath = path.join(__dirname, public || process.env.DATA_FOLDER)
+  const files = []
 
-  console.log('[PRELOAD][LOG] Removing old tar(s)')
-  const oldTars = await fetchTars(assetsPath)
-  await Promise.all(
-    oldTars.map((oldTar) => fs.unlink(path.join(assetsPath, oldTar)))
-  )
+  console.log(chalk`{bold.blue [LOAD][LOG]} Start computing hash of files`)
 
-  console.log('[LOAD][LOG] Start fetch assets')
-  const assets = await fetchAssets(assetsPath)
-
-  console.log('[LOAD][LOG] Start making tar(s) of assets')
-  let startTime = Date.now()
-  const tars = await Promise.all(
-    assets.map((asset) =>
-      tar
-        .c(
-          {
-            gzip: true,
-            file: path.join(assetsPath, asset + '.tgz')
-          },
-          [path.join(assetsPath, asset)]
-        )
-        .then((tar) => asset)
-    )
-  )
-  const tarTime = Date.now() - startTime
-  console.log('[LOAD][LOG] Tars builded in ' + tarTime + ' ms')
-
-  console.log('[LOAD][LOG] Start compute hash of assets')
-  startTime = Date.now()
-  global.assets = await Promise.all(
-    tars.map((assetName) => {
-      return hasha
-        .fromFile(path.join(assetsPath, assetName + '.tgz'), {
-          algorithm: 'md5'
-        })
-        .then(hash => ({
-          name: assetName,
-          folderPath: path.join(assetsPath, assetName),
-          tarPath: path.join(assetsPath, assetName + '.tgz'),
-          hash
-        }))
+  const hashStartTime = Date.now()
+  for await(const file of walk(publicPath)) {
+    const hash = await hasha.fromFile(file, {algorithm: 'sha1'})
+    files.push({
+      fullPath: file,
+      relativePath: file.replace(publicPath+'/', ''),
+      hash
     })
-  )
-  const hashTime = Date.now() - startTime
-  console.log('[LOAD][LOG] Assets hash computed in ' + hashTime + ' ms')
-
-  console.log('[STARTING][LOG] Ready to start server')
-  app.listen(process.env.PORT).then(() => {
-    console.log('[STARTING][INFO] Server started !')
-    console.log('[STARTED][INFO] You can stop it with Ctrl+C')
+  }
+  const fileHashTime = Date.now() - hashStartTime
+  console.log(chalk`{bold.blue [LOAD][LOG]} Files hash computed in {gray ${fileHashTime/1000}} seconds`)
+  
+  console.log(chalk`{bold.blue [STARTING][LOG]} Ready to start server`)
+  app.listen(process.env.PORT, ()=>{
+    console.log(chalk`{bold.blue [STARTING][INFO]} {green Server started !}`)
+    console.log(chalk`{bold.blue [INFO]} {green You can stop it with Ctrl+C}`)
   })
 }
 
--- src/utils/fetchAssets.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs').promises
-
-module.exports = async function fetchAssets (assetsPath) {
-  return fs.readdir(assetsPath, { withFileTypes: true }).then((files) => {
-    const folders = files.filter((file) => file.isDirectory())
-    return folders.map((folder) => folder.name)
-  })
-}
