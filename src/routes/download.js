519381a961fb1f09722802680a0d4e8316cb447b
loockeeer <lulu.r1313@gmail.com>
2020-12-05 13:12:49 +0100

    Create download route

--- src/routes/download.js
+++ src/routes/download.js
@@ -1,3 +1,15 @@
 module.exports = async (req, res) => {
-    
+  const relativePath = req.query.relativePath
+  if (!relativePath) {
+    return res
+      .status(400)
+      .send({ message: 'Missing relativePath in query parameters' })
+  }
+
+  const localFiles = req.app.get('files')
+
+  const file = localFiles.find((file) => file.relativePath === relativePath)
+  if (!file) { return res.status(404).send({ message: 'This file does not exists' }) }
+
+  return res.sendFile(file.fullPath)
 }
