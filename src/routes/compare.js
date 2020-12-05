40eec709ce275818aa0fc401b4e5bf48eee990dc
loockeeer <lulu.r1313@gmail.com>
2020-12-05 13:46:55 +0100

    Add pattern check to compare route

--- src/routes/compare.js
+++ src/routes/compare.js
@@ -1,8 +1,27 @@
+const isObject = require('../utils/isObject')
+
 module.exports = async (req, res) => {
   const localFiles = req.app.get('APP_files')
   const distantFiles = req.body
   if (!distantFiles) {
     return res.status(400).send({ message: 'Missing files array' })
+  }
+  if (!Array.isArray(distantFiles)) {
+    return res
+      .status(400)
+      .send({ message: 'Sent body does not match the requirements' })
+  }
+  const goodPattern = distantFiles.every(
+    (file) =>
+      isObject(file) &&
+      Object.prototype.hasOwnProperty.call(file, 'relativePath') &&
+      Object.prototype.hasOwnProperty.call(file, 'hash')
+  )
+
+  if (!goodPattern) {
+    return res
+      .status(400)
+      .send({ message: 'Sent array does not match required pattern' })
   }
   const toDownloadFiles = localFiles
     .filter((file) => {
