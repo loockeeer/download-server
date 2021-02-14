3b08af8c491a77f19bf3a49a6b31f506cb5fab38
loockeeer <lulu.r1313@gmail.com>
2021-02-14 23:00:25 +0100

    fix(test/index): fix the removing part enhancement(test/index): use a PORT variable and env

--- test/index.js
+++ test/index.js
@@ -6,32 +6,39 @@ const hasha = require('hasha')
 
 const walk = require('./walk')
 
-const folder = path.join(__dirname, 'public')
+const publicPath = path.join(__dirname, 'public')
+
+const PORT = process.env.PORT || 8080
 
 async function main () {
   const files = []
-  for await (const file of walk(folder)) {
+
+  for await (const file of walk(publicPath)) {
     const hash = await hasha.fromFile(file, { algorithm: 'sha1' })
     files.push({
       relativePath: file.replace(path.normalize(publicPath.endsWith('/') ? publicPath : `${publicPath}/`), ''),
       hash
     })
   }
+  console.log('done uh ')
   const toDownloadFiles = await axios({
-    url: 'http://localhost:8080/compare',
+    url: `http://localhost:${PORT}/compare`,
     method: 'POST',
     data: { files }
   }).then(res => res.data).catch(res => res.data)
-  console.log(toDownloadFiles)
-  for (const toDownload of toDownloadFiles) {
-    await fs.promises.mkdir(path.dirname(path.join(folder, toDownload.relativePath)), { recursive: true })
-    const file = fs.createWriteStream(path.join(folder, toDownload.relativePath))
 
-    http.get(`http://localhost:8080/download/${encodeURIComponent(toDownload.relativePath)}`, res => {
-      res.pipe(file)
-      console.log(`Starting fetch ${toDownload.relativePath}`)
-      res.on('end', () => console.log(`End fetching ${toDownload.relativePath}`))
-    })
+  for (const toDownload of toDownloadFiles) {
+    if(toDownload.op === "download") {
+      await fs.promises.mkdir(path.dirname(path.join(publicPath, toDownload.relativePath)), { recursive: true })
+      const file = fs.createWriteStream(path.join(publicPath, toDownload.relativePath))
+      http.get(`http://localhost:${PORT}/download/${encodeURIComponent(toDownload.relativePath)}`, res => {
+        res.pipe(file)
+
+      })
+    } else if (toDownload.op === "remove") {
+      console.log(toDownload.relativePath)
+      await fs.promises.unlink(path.join(publicPath, toDownload.relativePath))
+    }
   }
 }
 
