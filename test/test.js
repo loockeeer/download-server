8aecdbbe8c130f9497da4854fc56046cba6fc568
loockeeer <lulu.r1313@gmail.com>
2021-02-14 23:12:04 +0100

    feat(test/test): add some initialization process (public dir creation)

--- test/test.js
+++ test/test.js
@@ -1,31 +1,45 @@
 const { spawn, exec } = require('child_process')
+const { promisify } = require('util')
+const fs = require('fs/promises')
 
-// spawn the server process
-const server = spawn('node', ['src/index.js'])
+async function main() {
+    // create client public directory
+    await fs.mkdir('./test/public', { recursive: true })
 
-server.stdout.on('data', data => {
-    console.log("SERVER LOG :", data.toString())
-    if(data.includes('Server started')) {
-        // spawn the client process
-        const client = spawn('node', ['./test/index.js'])
-        client.stderr.on('data', d => console.log(d.toString()))
-        client.stdout.on('data', d => console.log(d.toString()))
-        client.on('close', () => {
-            // spawn the diff process
-            server.kill()
-            exec('diff -qr ./public ./test/public', (err, stdout) => {
-                console.log(stdout.toString())
+    // copy a git repository in the server's public directory
+    const REPOSITORY_URL = "https://github.com/loockeeer/minecraft-launcher"
 
-                if(stdout.length > 1) {
-                    console.error('The two public folders differ')
-                    process.exit(1)
-                } else {
-                    console.log('The two public folders are the same')
-                    process.exit(0)
-                }
+    await (promisify(exec)('git clone ' + REPOSITORY_URL + ' public'))
+    await (promisify(exec)('rm -rf public/.git'))
+
+    // spawn the server process
+    const server = spawn('node', ['src/index.js'])
+
+    server.stdout.on('data', data => {
+        console.log("SERVER LOG :", data.toString())
+        if(data.includes('Server started')) {
+            // spawn the client process
+            const client = spawn('node', ['./test/index.js'])
+            client.stderr.on('data', d => console.log(d.toString()))
+            client.stdout.on('data', d => console.log(d.toString()))
+            client.on('close', () => {
+                // spawn the diff process
+                server.kill()
+                exec('diff -qr ./public ./test/public', (err, stdout) => {
+                    console.log(stdout.toString())
+
+                    if(stdout.length > 1) {
+                        console.error('The two public folders differ')
+                        process.exit(1)
+                    } else {
+                        console.log('The two public folders are the same')
+                        process.exit(0)
+                    }
+                })
             })
-        })
-    }
-})
+        }
+    })
 
-server.stderr.on('data', d=> console.log(d.toString()))
+    server.stderr.on('data', d=> console.log(d.toString()))
+}
+main()
